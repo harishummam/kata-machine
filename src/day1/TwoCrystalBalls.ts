898248b6a2f6e319841ac75fa04b1235bb341762
harishummam <harishummambaluch@gmail.com>
2025-11-14 15:01:21 +0500

    Two Crystal Balls

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,21 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    const jmpAmount = Math.floor(Math.sqrt(breaks.length));
 
+    let i = jmpAmount;
+    for (; i < breaks.length; i += jmpAmount) {
+        if (breaks[i]) {
+            break;
+        }
+    }
+
+    i -= jmpAmount;
+
+    while (i < i + jmpAmount && i + jmpAmount < breaks.length) {
+        if (breaks[i]) {
+            return i;
+        }
+        i += 1;
+    }
+
+    return -1;
 }
